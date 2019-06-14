7151f4155bb69d3a5c980422c54d416269ad10fc
florianmac <nairolf3>
2019-06-14 09:55:02 +0200

    test

--- private/views/registreerBoddy.php
+++ private/views/registreerBoddy.php
@@ -1,6 +1,8 @@
 <link rel="stylesheet" type="text/css" href="../public/css/baseStyle.css">
 <form>
-  <input type="submit">	
+  <input type="text">
+  <input type="form">
+  <input type="submit">
 </form>
  <?php
 
